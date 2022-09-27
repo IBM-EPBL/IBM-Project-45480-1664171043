c013677789b7a7ea1eab672a3baea93a52ec770f
Fadal Razin <98084200+fadalrazin17@users.noreply.github.com>
2022-09-27 09:49:02 +0530

    Add files via upload

---
+++ Assessments/Fadal Razin/Assignment 1/concat,reverse,slicing.py
@@ -0,0 +1,17 @@
+# Concatenate
+
+x=str(input("Enter string1: "))
+y=str(input("Enter string2: "))
+z= x + y
+print(z)
+
+# Reverse
+
+text=x[::-1]
+print(text)
+
+# Slicing
+
+print(z[1:5])
+print(z[:5])
+print(z[1:4:7])
