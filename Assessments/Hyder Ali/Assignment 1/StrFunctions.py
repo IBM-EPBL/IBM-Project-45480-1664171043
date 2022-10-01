5c504200af3c405caf479f8a8ea5ff1d407ef696
Fadal Razin <98084200+fadalrazin17@users.noreply.github.com>
2022-10-01 12:24:03 +0530

    Create StrFunctions.py

---
+++ Assessments/Hyder Ali/Assignment 1/StrFunctions.py
@@ -0,0 +1,16 @@
+#CONCATENATE
+x = str(input("Enter str1 : "))
+y = str(input("Enter str2 : "))
+z = x + y
+print(z)
+
+
+#REVERSE
+text=x[::-1]
+print(text)
+
+
+#SLICING
+print(z[1:5])
+print(z[:5])
+print(z[1:4:7])
