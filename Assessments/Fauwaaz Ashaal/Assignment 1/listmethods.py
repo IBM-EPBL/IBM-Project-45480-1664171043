2824a824a88f75f1dcf437177c9a456a10d56547
Fadal Razin <98084200+fadalrazin17@users.noreply.github.com>
2022-10-01 12:06:56 +0530

    Create listmethods.py

---
+++ Assessments/Fauwaaz Ashaal/Assignment 1/listmethods.py
@@ -0,0 +1,25 @@
+#list and its operations
+
+list1=[101,104,106,108,115]
+print(list1)
+
+list1.insert(2,100)
+
+#list1.insert[101,104,106,108,115]
+
+print(list1)
+
+list1.remove(list1[0])
+print(list1)
+
+list1.append(150)
+print(list1)
+
+list1.sort()
+print(list1)
+
+list1.pop(-1)
+print(list1)
+
+list1.reverse()
+print(list1)
