75255734428cf254dbc00764ef14e6437273f09b
Fadal Razin <98084200+fadalrazin17@users.noreply.github.com>
2022-10-01 12:22:07 +0530

    Update ListMethods.py

--- Assessments/Hyder Ali/Assignment 1/ListMethods.py
+++ Assessments/Hyder Ali/Assignment 1/ListMethods.py
@@ -1,18 +1,21 @@
-N = int(input())
-Output = [];
-for i in range(0,N):
-    ip = input().split();
-    if ip[0] == "print":
-        print(Output)
-    elif ip[0] == "insert":
-        Output.insert(int(ip[1]),int(ip[2]))
-    elif ip[0] == "remove":
-        Output.remove(int(ip[1]))
-    elif ip[0] == "pop":
-        Output.pop();
-    elif ip[0] == "append":
-        Output.append(int(ip[1]))
-    elif ip[0] == "sort":
-        Output.sort();
-    else:
-        Output.reverse();
+list = [9, 7,8 ,12]
+
+list.insert(13,45)
+list.insert(1, 3)
+list.insert(22 , 6)
+print(list)
+
+list.remove(list[0])
+print(list)
+
+list.append(10)
+print(list)
+
+list.sort()
+print(list)
+
+list.pop(0)
+print(list)
+
+list.reverse()
+print(list)
